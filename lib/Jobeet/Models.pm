5d396476c9836dd27282405ba92aa6b16110c41a
koyo-miyamura <nexus9071@gmail.com>
2020-05-12 19:14:00 +0900

    4日目途中まで

--- lib/Jobeet/Models.pm
+++ lib/Jobeet/Models.pm
@@ -3,4 +3,23 @@ use strict;
 use warnings;
 use Ark::Models '-base';
 
+register Schema => sub {
+    my $self = shift;
+
+    my $conf = $self->get('conf')->{database}
+        or die 'require database config';
+
+    $self->ensure_class_loaded('Jobeet::Schema');
+    Jobeet::Schema->connect(@$conf);
+};
+
+autoloader qr/^Schema::/ => sub {
+    my ($self, $name) = @_;
+
+    my $schema = $self->get('Schema');
+    for my $t ($schema->sources) {
+        $self->register( "Schema::$t" => sub { $schema->resultset($t) });
+    }
+};
+
 1;
