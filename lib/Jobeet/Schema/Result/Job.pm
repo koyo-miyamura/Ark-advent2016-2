51bef28c64f048a47333ae44b1e31234bcee6dc2
koyo-miyamura <nexus9071@gmail.com>
2020-05-14 17:03:54 +0900

    10日目

--- lib/Jobeet/Schema/Result/Job.pm
+++ lib/Jobeet/Schema/Result/Job.pm
@@ -2,6 +2,8 @@ package Jobeet::Schema::Result::Job;
 use v5.20.3;
 use strict;
 use warnings;
+use Digest::SHA1 qw/sha1_hex/;
+use Data::UUID;
 use parent 'Jobeet::Schema::ResultBase';
 use Jobeet::Models;
 
@@ -102,7 +104,30 @@ sub insert {
     my $self = shift;
 
     $self->expires_at( models('Schema')->now->add( days => models('conf')->{active_days} ) );
+    $self->token( sha1_hex(Data::UUID->new->create) );
+
     $self->next::method(@_);
+}
+
+sub is_expired {
+    my ($self) = @_;
+    $self->days_before_expired < 0;
+}
+
+sub days_before_expired {
+    my ($self) = @_;
+    my $now = models('Schema')->now;
+    $self->expires_at->delta_days($now)->in_units('days');
+}
+
+sub expires_soon {
+    my ($self) = @_;
+    $self->days_before_expired < 5;
+}
+
+sub publish {
+    my ($self) = @_;
+    $self->update({ is_activated => 1 });
 }
 
 __PACKAGE__->set_primary_key('id');
